4176b2a71bb3eaef7127bc9814a40a2ca7a3e908
KevinrealG <kevinsossav@gmail.com>
2020-09-18 07:50:52 -0500

    Agregue mas texto al input

--- first.py
+++ first.py
@@ -7,7 +7,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Enter what- ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 number = re.findall(b'>([0-9]+)', html)
